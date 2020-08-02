bf7ef61bd3feba91c9b45673a2245f836f8707b2
Nadav.R <3xpl0it2c@gmail.com>
2020-08-02 22:44:24 +0300

    I Accidently created the repository code inside the functions directory for the user sign up functionality. This commit moves that code into the repository but sadly there is no new code yet. I might finish it this weekend, when I have more time. Meanwhile, no body looks at this code and nobody reads this commit, Who cares.

--- functions/Users/signUp.ts
+++ functions/Users/signUp.ts
@@ -1,24 +1,37 @@
-import { DatabasePoolType, sql } from 'slonik';
-import { User } from '@interfaces';
-import { hashPassword } from '@lib';
-import {Logger} from 'log4js';
+import { declareAppModule } from '@lib';
+import {Context} from 'koa';
+import * as Joi from '@hapi/joi';
 
-export const signUp = (user: User) => async (
-    slonik: DatabasePoolType,
-    logger: Logger,
-) => {
-    const { firstName, lastName, email, password } = user;
+const handler = async (ctx: Context) => {
+    const schema = Joi.object({
+        name: Joi.string()
+            .alphanum()
+            .max(30)
+            .min(3)
+            .required(),
+        lastName: Joi.string()
+            .alphanum()
+            .max(30)
+            .min(2)
+            .required(),
+        email: Joi.string()
+            .min(5)
+            .email()
+            .required(),
+        password: Joi.string()
+            .min(8)
+            .max(24)
+            .required(),
+    });
 
     try {
-        const hashedPass = hashPassword(password);
-        slonik.connect(async (conn) => {
-            await conn.query(sql`
-                             INSERT INTO users
-                             (firstName, lastName, email, password)
-                             VALUES
-                             (${sql.array([firstName, lastName, email, hashedPass], 'text')})
-                             `);
-        });
+        const newUser = await schema.validateAsync(ctx.query);
     } catch (why) {}
 };
+
+export default declareAppModule({
+    httpMethod: 'POST',
+    path: '/user/signup',
+    handler,
+});
 
--- repository/users/writeUser.ts
+++ repository/users/writeUser.ts
@@ -1,4 +1,29 @@
 import { DatabasePoolType, sql } from 'slonik';
 import { User } from '@interfaces';
+import { hashPassword } from '@lib';
+import {Logger} from 'log4js';
 
+
+// SELECT SCOPE_IDENTITY() 
+export const signUp = (user: User) => async (
+    slonik: DatabasePoolType,
+    logger: Logger,
+) => {
+    const { firstName, lastName, email, password } = user;
+
+    try {
+        const hashedPass = hashPassword(password, logger);
+
+        slonik.connect(async (conn) => {
+            await conn.query(sql`
+                             INSERT INTO users
+                             (firstName, lastName, email, password)
+                             VALUES
+                             (${sql.array([firstName, lastName, email, hashedPass], 'text')})
+                             `);
+
+            logger.log(`Inserted new User to database`);
+        });
+    } catch (why) {}
+};
 
