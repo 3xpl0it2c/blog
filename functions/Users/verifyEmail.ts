b4cd9efc2aa03eb8141d0da3784724f7ddac482b
Nadav.R <3xpl0it2c@gmail.com>
2020-08-09 12:22:08 +0300

    I accidently leaked my database password... This commit removes every config file from the repo And adds them to the gitignore, so I won't commit them again.

---
+++ functions/Users/verifyEmail.ts
@@ -0,0 +1,16 @@
+/*
+Upon a POST to /users/verify_email
+This function should:
+
+1.Aqcuire a token from the url.
+2.Verify it for safety.
+3.Using the repository:
+    Find a user with the corresponding token.
+    If the difference between the date of registration and now is bigger than 1 day:
+        Delete the user's account.
+        And Respond with a message explaining the timeout.
+    Else:
+        Set the user as verified.
+        Respond with a successful message.
+
+*/
